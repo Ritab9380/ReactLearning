53168a2754e635c0dbd051022edd95e86f742128
Ritab9380 <ritabratasarkar10@gmail.com>
2024-08-23 18:46:55 +0530

    Updated the coes issue

--- src/components/Body.js
+++ src/components/Body.js
@@ -16,7 +16,7 @@ const Body=()=>{
   },[]);
 
   const fetchData = async ()=>{
-    const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=9.91850&lng=76.25580&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"); 
+    const data= await fetch("https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=9.91850&lng=76.25580&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"); 
 
     const json= await data.json();
     //console.log(json);
